ef63acb4a4573fdb1187e79f32a95786a90ac283
choi1324 <chj4535@naver.com>
2022-07-19 22:47:07 +0900

    feat: 로고 추가 및 파일로더 형식, 이미지 타입 선언

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import react_ico from '@/assets/images/react_ico.svg';
 
 const HEADER_TITLE = '헤더 타이틀'
 
 function Header() {
     return (
         <div id="header">
+            <img src={react_ico} />
             <strong>{HEADER_TITLE}</strong>
         </div>
     )
